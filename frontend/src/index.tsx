9cc72b150aeee4bac658bcb1e397424e573a76c1
sergey <korgeflus@mail.ru>
2023-07-31 20:15:52 +0300

    Страница логина

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,15 +4,18 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import { ChakraProvider } from "@chakra-ui/react"
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
-);
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- frontend/src/pages/Login/Login.tsx
+++ frontend/src/pages/Login/Login.tsx
@@ -1,6 +1,59 @@
 import React, { FC, memo } from "react"
-import { RootStyled } from "./styles"
+import {
+  Box,
+  Button,
+  Container,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+  Link,
+  Stack,
+  Text,
+} from "@chakra-ui/react"
+import { Logo } from "./Logo"
+import { PasswordField } from "./PasswordField"
 
 export const Login: FC = memo(() => {
-  return <RootStyled>ssaffa</RootStyled>
+  return (
+    <Container
+      maxW="lg"
+      py={{ base: "12", md: "24" }}
+      px={{ base: "0", sm: "8" }}
+    >
+      <Stack spacing="8">
+        <Stack spacing="6">
+          <Logo />
+          <Stack spacing={{ base: "2", md: "3" }} textAlign="center">
+            <Heading size={{ base: "xs", md: "sm" }}>
+              Войдите в свой аккаунт
+            </Heading>
+            <Text color="fg.muted">
+              У вас нет аккаунта? <Link href="#">Зарегистрируйтесь</Link>
+            </Text>
+          </Stack>
+        </Stack>
+        <Box
+          py={{ base: "0", sm: "8" }}
+          px={{ base: "4", sm: "10" }}
+          bg={{ base: "transparent", sm: "bg.surface" }}
+          boxShadow={{ base: "none", sm: "md" }}
+          borderRadius={{ base: "none", sm: "xl" }}
+        >
+          <Stack spacing="6">
+            <Stack spacing="5">
+              <FormControl>
+                <FormLabel>Логин</FormLabel>
+                <Input id="email" type="email" />
+              </FormControl>
+              <PasswordField />
+            </Stack>
+            <Stack spacing="6">
+              <Button>Войти</Button>
+            </Stack>
+          </Stack>
+        </Box>
+      </Stack>
+    </Container>
+  )
 })
